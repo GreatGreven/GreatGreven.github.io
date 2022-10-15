ef78367e48786d549833e3669640573b291b9c21
Eric Grevillius <34370827+GreatGreven@users.noreply.github.com>
2022-10-16 00:55:07 +0200

    Worked on styling and transistion toggle in CV-section

--- script.js
+++ script.js
@@ -11,12 +11,18 @@ const theme = localStorage.getItem('theme')
 const bodyClass = document.body.classList;
 bodyClass.add(theme);
 
-function toggleTheme() {
+document.getElementById('themeButton').onclick = () => {
   const current = localStorage.getItem('theme');
   const next = themeMap[current];
-
+  
   bodyClass.replace(current, next);
   localStorage.setItem('theme', next);
 }
 
-document.getElementById('themeButton').onclick = toggleTheme;
+let articleTitles = document.querySelectorAll('div.articleTitle')
+for (let i = 0; i < articleTitles.length; i++) {
+  articleTitles[i].addEventListener('click', (e) => {
+    e.target === e.currentTarget ? e.target.parentNode.classList.toggle('closed') : e.target.parentNode.parentNode.classList.toggle('closed')
+  }, false)
+}
+
